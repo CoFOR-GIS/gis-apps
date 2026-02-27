56532e74ba75abb463efdcb27211b09b9d89574a
CoFOR-GIS <emartinez@duck.com>
2026-02-27 15:19:20 -0600

    Update config.js

--- WaterMeterEditor/js/config.js
+++ WaterMeterEditor/js/config.js
@@ -14,7 +14,7 @@ var APP_CONFIG = {
   PORTAL_URL: "https://fairoaksranch.maps.arcgis.com",
 
   // -- OAuth 2.0 (redirect-based, no popup) --
-  OAUTH_APP_ID: "iBmou4LFG3czFKPg",
+  OAUTH_APP_ID: "JzMBy8soKL0MZN3C",
 
   // -- Data Source --
   // Water Meters - Operational Edit View (Utilities - Internal)
